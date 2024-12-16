0fb505cf5d1e85d5565933de81e3ed5328f6c73a
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2024-12-16 18:06:26 +0200

    created project structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  ignorePatterns: ["static", ".eslintrc.cjs", "node_modules"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
+    "prefer-const": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".jsx", ".tsx", ".ts", ".js"],
+      },
+      alias: {
+        extensions: [".jsx", ".tsx", ".ts", ".js"],
+        map: [
+          ["assets", "./src/assets"],
+          ["components/*", "./src/components"],
+          ["constants", "./src/constants"],
+          ["pages", "./src/pages"],
+          ["types", "./src/types"],
+          ["utils", "./src/utils"],
+        ],
+      },
+    },
+    react: {
+      version: "detect",
+    },
+  },
+};
