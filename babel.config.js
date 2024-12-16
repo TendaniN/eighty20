801fbd5063e20bd3e6af1acfe103f82d88459a38
Tendani Netshitenzhe <tbnetshitenzhe@gmail.com>
2024-12-16 19:23:06 +0200

    fixed build

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            esmodules: true,
+          },
+        },
+      ],
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+    ],
+    env: {
+      test: {
+        plugins: [
+          "require-context-hook"
+        ],
+      },
+    },
+  };
+  
